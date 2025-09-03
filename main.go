dbf9317cb33c10f3b241793e52945ed502d52f39
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-03 16:20:10 +0300

    8

--- main.go
+++ main.go
@@ -1,54 +1,27 @@
 package main
 
 import (
-	"cmp"
 	"fmt"
-
-	"sync"
+	"log"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	safemap1 := new(sync.Map)
-	var mt sync.Mutex
-
-	map1 := make(map[string]string)
-	map2 := make(map[int]int)
-
-	wg.Add(3)
-	for i := 0; i < 3; i++ {
-		go func(i int) {
-			mt.Lock()
-			map2[3] = i
-			mt.Unlock()
-		}(i)
+	var number int64
+	var requiredBit int
+	value := true
+	fmt.Println("enter the number, required bit and Value you would like to set")
+	_, err := fmt.Scan(&number, &requiredBit, &value)
+	if err != nil {
+		log.Fatalf("data input error: %v", err)
 	}
-
-	for i := 1; i <= 3; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			SafeWriteToMap(map1, &mt)
-		}(i)
-		go func(i int) {
-			defer wg.Done()
-			safemap1.Store(i, struct{}{})
-
-			mt.Lock()
-			map2[2] = i
-			mt.Unlock()
-		}(i)
-	}
-	wg.Wait()
-	fmt.Printf("safemap: %v\nmap1: %v\nmap2: %v", safemap1, map1, map2)
+	fmt.Println("\n", bitConverser(number, requiredBit-1, value))
 }
 
-func SafeWriteToMap[T cmp.Ordered, V any](mp map[T]V, mt *sync.Mutex) {
+func bitConverser(number int64, bit int, value bool) int64 {
+	mask := int64(1 << bit)
+	if value {
+		return number | mask
+	}
+	return number &^ mask
 
-	mt.Lock()
-	var key T
-	var value V
-	fmt.Scan(&key, &value)
-	mp[key] = value
-	mt.Unlock()
 }

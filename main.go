5fa476685b0ae7b2ba74dcb4aeb397f4529b0b89
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-03 18:29:20 +0300

    back to 11

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 )
 
 func main() {
-	A := []string{"cat", "cat", "dog", "cat", "tree"}
-	checker := make(map[string]struct{})
-	NewAsset := make([]string, 0)
+	A := []int{1, 2, 3}
+	B := []int{2, 3, 4}
+	checker := make(map[int]struct{})
+	intersection := make([]int, 0)
 	for _, v := range A {
+		checker[v] = struct{}{}
+	}
+	for _, v := range B {
 		_, ok := checker[v]
-		if !ok {
-			NewAsset = append(NewAsset, v)
-			checker[v] = struct{}{}
+		if ok {
+			intersection = append(intersection, v)
 		}
 	}
-
-	fmt.Println(NewAsset)
+	fmt.Println(intersection)
 }

83768b8a56bb4f9b689031eb4843d02d5265827a
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-03 18:05:40 +0300

    11

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	orderedTemp := make(map[int][]string)
-	temp := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
-	for _, v := range temp {
-		orderedTemp[int(v/10)*10] = append(orderedTemp[int(v/10)*10], fmt.Sprintf("%.1f", v))
-		// orderedTemp[int(v/10)*10] = append(orderedTemp[int(v/10)*10], v) в случае если можно пренебречь отображением 0 после запятой
+	A := []int{1, 2, 3}
+	B := []int{2, 3, 4}
+	checker := make(map[int]struct{})
+	intersection := make([]int, 0)
+	for _, v := range A {
+		checker[v] = struct{}{}
 	}
-	fmt.Println(orderedTemp)
+	for _, v := range B {
+		_, ok := checker[v]
+		if ok {
+			intersection = append(intersection, v)
+		}
+	}
+	fmt.Println(intersection)
 }

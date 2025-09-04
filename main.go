f85a833a1c4cd64fbe35157cc9d3da3c7eb5b7e1
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-04 14:40:35 +0300

    14

--- main.go
+++ main.go
@@ -2,25 +2,44 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
-
-	fmt.Println(mathSwap(5, 10))
-	fmt.Println(bitSwap(5, 10))
-
+	data := []interface{}{
+		"asdfa",
+		3241,
+		true,
+		make(chan any),
+		nil,
+	}
+	for _, v := range data {
+		typeDeterminator(v)
+	}
 }
 
-func mathSwap(a, b int) (newA, newB int) {
-	a += b
-	b = a - b
-	a -= b
-	return a, b
-}
+func typeDeterminator(data any) any {
+	value := reflect.ValueOf(data)
+	kind := value.Kind()
 
-func bitSwap(a, b int) (newA, newB int) {
-	a ^= b
-	b ^= a
-	a ^= b
-	return a, b
+	switch kind {
+	case reflect.Int:
+		fmt.Println("data type is integer")
+		determinedData := data.(int)
+		return determinedData
+	case reflect.String:
+		fmt.Println("data type is string")
+		determinedData := data.(string)
+		return determinedData
+	case reflect.Bool:
+		fmt.Println("data type is bool")
+		determinedData := data.(bool)
+		return determinedData
+	case reflect.Chan:
+		cType := value.Type().String()
+		fmt.Printf("data type is %s \n", cType)
+		return data
+	}
+	fmt.Println("type can't be determinated")
+	return nil
 }

1ff0b110b0c5593ca347a56d101926bf5c6fe563
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-04 12:30:01 +0300

    13

--- main.go
+++ main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	orderedTemp := make(map[string][]string)
-	negZero := math.Copysign(0, -1)
-	temp := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5, -5, 5}
-	for _, v := range temp {
-		if v < 0 && v > -10 {
-			orderedTemp[fmt.Sprintf("%.0f", negZero)] = append(orderedTemp[fmt.Sprintf("%.0f", negZero)], fmt.Sprintf("%.1f", v))
-			continue
-		}
-		orderedTemp[fmt.Sprintf("%.0f", math.Floor(v/10)*10)] = append(orderedTemp[fmt.Sprintf("%.0f", math.Floor(v/10)*10)], fmt.Sprintf("%.1f", v))
-		// orderedTemp[int(v/10)*10] = append(orderedTemp[int(v/10)*10], v) в случае если можно пренебречь отображением 0 после запятой
-	}
-	fmt.Println(orderedTemp)
+
+	fmt.Println(mathSwap(5, 10))
+	fmt.Println(bitSwap(5, 10))
+
+}
+
+func mathSwap(a, b int) (newA, newB int) {
+	a += b
+	b = a - b
+	a -= b
+	return a, b
+}
+
+func bitSwap(a, b int) (newA, newB int) {
+	a ^= b
+	b ^= a
+	a ^= b
+	return a, b
 }

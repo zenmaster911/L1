be49a373c73b3ca2fc2e2afb0965af3eab5efa45
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-10 12:59:01 +0300

    20

--- main.go
+++ main.go
@@ -4,21 +4,65 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
 
 func main() {
+	fmt.Println("choose func you would like to use:\n print 1 to use func with Slices\n print 2 to avoid using slices")
+	var choise int
+	fmt.Scan(&choise)
+	switch choise {
+	case 1:
+		withSliceslib()
+	case 2:
+		withoutSliceslib()
+	}
+}
+
+func withSliceslib() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanLines)
-	fmt.Println("to stop scanning enter \"end\"")
+	fmt.Println("to stop scanning print \"end\"")
 	for scanner.Scan() {
-		initialWord := scanner.Text()
-		if initialWord == "end" {
+		initialPhrase := scanner.Text()
+		if initialPhrase == "end" {
 			break
 		}
-		newWord := make([]rune, len(initialWord))
-		for i, r := range initialWord {
-			newWord[len(newWord)-1-i] = r
-		}
-		fmt.Println(string(newWord))
+		slice := strings.Split(initialPhrase, " ")
+		slices.Reverse(slice)
+		newPhrase := strings.Join(slice, " ")
+		fmt.Println(newPhrase)
 	}
+}
+
+func withoutSliceslib() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+	fmt.Println("to stop scanning print \"end\"")
+
+	for scanner.Scan() {
+		inputData := scanner.Text()
+		fmt.Println(wordOrderReverse(inputData))
+
+	}
+}
+
+func wordOrderReverse(str string) string {
+	var newstr []byte
+	var end int
+	end = len(str)
+	for b := end - 1; b >= 0; b-- {
+		if str[b] == byte(' ') {
+			newstr = append(newstr, str[b:end]...)
+			end = b
+		}
+		if b == 0 {
+			newstr = append(newstr, byte(' '))
+			newstr = append(newstr, str[b:end]...)
+		}
+	}
+	newstr = newstr[1:]
+
+	return string(newstr)
 }

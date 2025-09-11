b1d0348f0b6d2f0d31989e4163f65942dd3b9f55
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-11 12:46:12 +0300

    22

--- main.go
+++ main.go
@@ -2,69 +2,54 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 )
 
-type exclaimer interface {
-	exclaim()
-}
-
-type example1 struct{}
-
-type example2 struct{}
-
-type balerina struct{}
-
-func (ex example1) experiment() {
-	fmt.Println("Меня сейчас адаптируют")
-}
-
-func (ex example2) experimentResult() {
-	fmt.Println("меня адпатировали")
-}
-
-func (bal balerina) piruet() {
-	fmt.Println("А я - балерина!")
-}
-
-type adapter1 struct {
-	*example1
-}
-
-type adapter2 struct {
-	*example2
-}
-
-type baleriner struct {
-	*balerina
-}
-
-func (a1 adapter1) exclaim() {
-	a1.experiment()
-}
-
-func NewAdapter1(ex *example1) *adapter1 {
-	return &adapter1{ex}
-}
-
-func (a2 adapter2) exclaim() {
-	a2.experimentResult()
-}
-
-func NewAdapter2(ex *example2) *adapter2 {
-	return &adapter2{ex}
-}
-
-func (b baleriner) exclaim() {
-	b.piruet()
-}
-
-func NewBaleriner(bal *balerina) *baleriner {
-	return &baleriner{bal}
-}
-
 func main() {
-	madHouse := []exclaimer{NewAdapter1(&example1{}), NewAdapter2(&example2{}), NewBaleriner(&balerina{})}
-	for _, ward := range madHouse {
-		ward.exclaim()
+	for {
+
+		var a, b big.Int
+		var operator string
+		var stop string
+		fmt.Println("желаете продолжить? y/n")
+		fmt.Scan(&stop)
+		if stop == "n" {
+			break
+		} else if stop != "y" {
+			fmt.Println("команда неясна, попробуйте снова")
+		}
+		fmt.Println("Введите первое число")
+
+		if _, err := fmt.Scan(&a); err != nil {
+			fmt.Printf("first number scanning error: %v\n", err)
+			continue
+		}
+
+		fmt.Println("Введите один из следующих операндов: '+' '-' '*' '/'")
+		if _, err := fmt.Scan(&operator); err != nil {
+			//fmt.Errorf()
+			fmt.Printf("operator scanning error: %v\n", err)
+			continue
+		}
+
+		fmt.Println("Введите второе число")
+		if _, err := fmt.Scanln(&b); err != nil {
+			fmt.Printf("second number scanning error: %v", err)
+			continue
+		}
+
+		switch operator {
+		case "+":
+			fmt.Printf("результат операции: %v\n", a.Add(&a, &b))
+		case "*":
+			fmt.Printf("результат операции: %v\n", a.Mul(&a, &b))
+		case "-":
+			fmt.Printf("результат операции: %v\n", a.Sub(&a, &b))
+		case "/":
+			fmt.Printf("результат операции: %v\n", a.Div(&a, &b))
+		default:
+			fmt.Println("invalid operator")
+		}
+
 	}
 }

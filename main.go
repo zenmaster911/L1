1aaf5a8fa0354824bdc3bc6fa92861d77c6c66f4
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-17 19:57:16 +0300

    25

--- main.go
+++ main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"sync"
+	"time"
 )
 
-type Point struct {
-	x float64
-	y float64
-}
-
-func newPoint(x, y float64) *Point {
-	return &Point{x: x, y: y}
-}
-
-func (p Point) distance(point Point) float64 {
-	return math.Sqrt(math.Pow(p.x-point.x, 2) + math.Pow(p.y-point.y, 2))
-}
-
 func main() {
-	a := newPoint(2, 2)
-	b := newPoint(5, 2)
-	fmt.Println(a.distance(*b))
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 5; i++ {
+			fmt.Println(i + 1)
+			Sleep(2)
+		}
+	}()
+	wg.Wait()
+}
+
+func Sleep(duration time.Duration) {
+	timer := time.NewTimer(duration * time.Second)
+	<-timer.C
 }
